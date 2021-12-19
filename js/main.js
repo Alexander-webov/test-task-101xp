afb110caec2871cf9d7f8c938a8d20bd26643398
Alexander-webov <vilmen37@gmail.com>
2021-12-19 17:16:25 +0300

    final

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $('.chars__slider').slick({
+        infinite: true,
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    });
+    $("a[href^='#']").click(function () {
+        var _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
+    });
+
+});
